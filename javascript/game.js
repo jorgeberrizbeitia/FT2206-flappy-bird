5998e00c561b44a45a3c564e6c33d2160ce57b44
jorgeberrizbeitia <jorgeberrizbeitia@gmail.com>
2022-07-07 10:36:27 +0200

    initial structure

---
+++ javascript/game.js
@@ -0,0 +1,11 @@
+// aqui estará la clase del juego con todos los elementos que afecte el juego
+
+class Game {
+  constructor() {
+    // todas las propiedades del juego (ej: todas las variables globales de pong)
+  }
+
+
+  // .todos los metodos del juego (ej: las funciones que teniamos ayer en el pong)
+
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,19 +1,26 @@
+// Main solo será para manejo de estados y manipulacion de DOM
 // * GLOBAL VARIABLES
 
+// seccion de canvas y ctx
+const canvas = document.querySelector("#my-canvas");
+const ctx = canvas.getContext("2d");
 
-
-
+// elementos de DOM
+const splashScreenDOM = document.querySelector("#splash-screen");
+const startBtn = document.querySelector("#start-btn");
 
 // * STATE MANAGEMENT FUNCTIONS
 
+const startGame = () => {
+  console.log("intentando inicar el juego");
+  splashScreenDOM.style.display = "none";
+  canvas.style.display = "block";
 
-
-
-
+  // ... aqui es donde deberia iniciar el juego.
+  const game = new Game()
+  console.log(game)
+};
 
 // * ADD EVENT LISTENERS
 
-
-
-
-
+startBtn.addEventListener("click", startGame);
